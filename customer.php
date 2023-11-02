b81bf2f35c7a0831ed1ec6b42e49690c92e73a12
nabilabalqiss <nabila11balqis@gmail.com>
2023-11-02 22:43:52 +0700

    first commit

---
+++ customer.php
@@ -0,0 +1,15 @@
+<!--Content-->
+<div class="col-lg-9 mt-2">
+      <div class="card">
+  <div class="card-header">
+    Customer
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Ini adalah bagian customer</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quos molestias, nostrum vero, voluptas voluptatem culpa consectetur assumenda reiciendis hic in temporibus possimus. Explicabo laboriosam aperiam ut animi sint natus!</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div> 
+<!--End Content-->
